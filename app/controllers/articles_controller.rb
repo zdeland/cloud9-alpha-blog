69c84edd5281bea6e7e8a6227faf6559d5a117ee
Zachary <zrdeland@gmail.com>
2018-10-20 21:07:22 +0000

    Fix create, add flash and error msgs, created show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,24 @@
 class ArticlesController < ApplicationController
  def new 
-  @article = Article.new
+  @art = Article.new
  end 
  
  def create 
    #retrieve form filed values
    #render plain: params[:article].inspect 
-   
    #must whitelist the params to create a new article
    @art = Article.new(article_params)
-   @art.save
-   redirect_to articles_show(@art)
+   
+   if @art.save
+      flash[:notice] = "Article was successfully created!"
+      redirect_to article_path(@art)
+   else
+      render 'new' # renders new template
+   end 
+ end
+ 
+ def show
+    @art = Article.find(params[:id])
  end
  
  private 
