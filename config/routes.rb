99f7d2c25d61deb89d6302af11097298400e6075
Zachary <zrdeland@gmail.com>
2018-11-13 00:15:37 +0000

    Signup users from ui

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :articles # creates CRUD routes 
+  resources :users, except: :new #creates CRUD routes except the custom new route
   
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
   
   
 end
