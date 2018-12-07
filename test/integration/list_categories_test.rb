9681551398b13a0e3a6cc36a3315973d929835f2
Zachary <zrdeland@gmail.com>
2018-12-07 01:21:14 +0000

    Added categories feature, integration tests

---
+++ test/integration/list_categories_test.rb
@@ -0,0 +1,29 @@
+require "test_helper"
+
+class ListCategoriesTest < ActionDispatch::IntegrationTest
+   def setup
+    @category = Category.create(name:"sports")
+    @category2 = Category.create(name:"art")
+   end
+   
+   
+   test "Should show categories listing" do
+       get categories_path
+       assert_template "categories/index"
+       assert_select "a[href=?]",category_path(@category),@category.name
+       assert_select "a[href=?]",category_path(@category2),@category2.name
+       
+   end
+   
+   test "invalid category submission results in failure" do
+       get new_category_path
+       assert_template 'categories/new'
+       assert_no_difference 'Category.count' do
+        post categories_path, params: {category: {name: ""}}
+       end 
+       assert_template 'categories/new'
+       assert_select "h2.card-title"
+       assert_select "div.card-body"
+   end
+   
+end
