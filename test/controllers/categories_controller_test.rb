8048acc41ddec372bd8fe804eebff1daf32c1f03
Zachary <zrdeland@gmail.com>
2018-12-08 02:00:40 +0000

    Only admins can create categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,8 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
    
    def setup    
       @category = Category.create(name: "sports")
+      @user = User.create(username:"John", email: "john@email.com",password: "password",admin:true)
+      
    end
    
    test "Should get categories index" do
@@ -11,7 +13,8 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
       assert_response :success
    end 
    
-   test "Should get categories new" do
+   test "Should sign in as admin" do
+      sign_in_as(@user)
       get new_category_path
       assert_response :success 
    end 
@@ -20,4 +23,12 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
       get category_path(@category)
       assert_response :success  
    end 
+   
+   test "Should redirect create when admin not logged in" do
+      assert_no_difference "Category.count" do
+         post categories_path, params: {category:{name:"sports"}}
+      end
+      assert_redirected_to categories_path
+   end
+   
 end
--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class ListCategoriesTest < ActionDispatch::IntegrationTest
    def setup
+    @user = User.create(username:"John", email: "john@email.com",password: "password",admin:true)
     @category = Category.create(name:"sports")
     @category2 = Category.create(name:"art")
    end
@@ -16,6 +17,7 @@ class ListCategoriesTest < ActionDispatch::IntegrationTest
    end
    
    test "invalid category submission results in failure" do
+       sign_in_as(@user)
        get new_category_path
        assert_template 'categories/new'
        assert_no_difference 'Category.count' do
